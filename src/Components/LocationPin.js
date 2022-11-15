a404537b94f12eba448eb05328be33e452087653
esaltzm <elisaltzman@gmail.com>
2022-11-15 13:39:18 -0500

    added hover thumbnails to map icons

--- src/Components/LocationPin.js
+++ src/Components/LocationPin.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Icon } from '@iconify/react'
-import locationIcon from '@iconify/icons-mdi/map-marker'
 
 export default function LocationPin({ photo, setPhotoInfo }) {
+    const [isHovering, setIsHovering] = useState(false)
     return (
         <div>
-            <div className="pin">
-                <Icon icon={locationIcon} className="pin-icon" onClick={() => {setPhotoInfo(photo)}} />
-            </div>
+            {isHovering ?
+                <div className='pin-hover' onMouseOut={() => { setIsHovering(false) }} onClick={() => { setPhotoInfo(photo) }}>
+                    <img src={photo.url} style={{height:'90px', border:'solid red 4px', borderRadius:'3px'}}/>
+                </div> :
+                <div className='pin'>
+                    <Icon
+                        icon={'material-symbols:image'}
+                        className='pin-icon'
+                        onClick={() => { setPhotoInfo(photo) }}
+                        onMouseOver={() => { setIsHovering(true) }}
+                    />
+                </div>
+            }
         </div>
     )
 }
