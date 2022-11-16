6c2891c172515d29f4f2efbe397c86feab52b35f
esaltzm <elisaltzman@gmail.com>
2022-11-16 14:48:52 -0500

    ready to deploy

--- src/Components/UploadPhotos.js
+++ src/Components/UploadPhotos.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default function UploadPhotos({setPhotoFiles}) {
+export default function UploadPhotos({ setPhotoFiles }) {
     const navigate = useNavigate()
     const selectNewPhotos = (e) => {
-		setPhotoFiles([...e.target.files])
-	}
+        setPhotoFiles([...e.target.files])
+    }
     return (
         <div className='upload-photos'>
             <input type='file' multiple accept='image/*, .heic' onChange={(e) => {
                 selectNewPhotos(e)
                 navigate('/')
-                }}></input>
+            }}></input>
         </div>
     )
 }
-
-// https://stackoverflow.com/questions/57127365/make-html5-filereader-working-with-heic-files
